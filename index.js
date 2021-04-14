97d4a90dcac88f2927b53e404828e8fa1ccc1ed0
Piyush <piyush41y08h@gmail.com>
2021-04-14 12:04:26 +0530

    Fix auth

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
--- middlewares/requiresAuthentication.js
+++ middlewares/requiresAuthentication.js
@@ -1,5 +1,4 @@
 function requiresAuthentication(req, res, next) {
-  console.log(req.cookies);
   if (!req.user)
     return res.status(401).json({
       message: "You must be logged in to perform this operation",
