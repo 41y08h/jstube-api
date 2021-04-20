21958ec50f892d83ed6a9e0cc4b18434f38e916d
Piyush <piyush41y08h@gmail.com>
2021-04-20 17:06:48 +0530

    Fix google login

--- src/handlers/videos/getVideos.js
+++ src/handlers/videos/getVideos.js
@@ -1,7 +1,7 @@
 const Video = require("../../models/Video");
 
 async function getVideos(req, res) {
-  const videos = await Video.find().populate("_user", "name");
+  const videos = await Video.find().populate("_user", "name, picture");
   res.json(videos);
 }
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,12 +2,23 @@ const router = require("express").Router();
 const authRoute = require("../middlewares/authRoute");
 const getUser = require("../handlers/auth/getUser");
 const logout = require("../handlers/auth/logout");
-const googleLogin = require("../handlers/auth/googleLogin");
 const asyncHandler = require("express-async-handler");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 router
   .get("/logout", asyncHandler(logout))
   .get("/user", authRoute, asyncHandler(getUser))
-  .get(["/google", "/google/callback"], asyncHandler(googleLogin));
+  .get(
+    ["/google", "/google/callback"],
+    passport.authenticate("google", {
+      session: false,
+      scope: ["email", "profile"],
+    }),
+    (req, res) => {
+      const token = jwt.sign(req.user.id, process.env.JWT_SECRET);
+      res.redirect(`${process.env.CLIENT_URL}/auth-complete?token=${token}`);
+    }
+  );
 
 module.exports = router;
