5db407e13c7a7f8818f45c92c9db63c90130cfcf
Piyush <piyush41y08h@gmail.com>
2021-05-15 11:35:51 +0530

    Remove comment replies from base comment

--- src/controllers/comment/getAll.ts
+++ src/controllers/comment/getAll.ts
@@ -3,6 +3,9 @@ import Comment from "../../models/Comment";
 
 export default asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  const comments = await Comment.find({ _video: videoId });
+  const comments = await Comment.find({
+    _video: videoId,
+    _replyTo: { $exists: false },
+  });
   res.json(comments);
 });
