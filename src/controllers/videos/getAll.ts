2b9b8c3dfce104563072c6bd9386f665a463213f
Piyush <piyush41y08h@gmail.com>
2021-05-15 10:31:27 +0530

    Fix video controllers

--- src/controllers/videos/getAll.ts
+++ src/controllers/videos/getAll.ts
@@ -2,5 +2,5 @@ import asyncHandler from "../../lib/asyncHandler";
 import Video from "../../models/Video";
 
 export default asyncHandler(async (req, res) => {
-  res.json(await Video.find());
+  res.json(await Video.find().populate("_user", "name picture"));
 });
--- src/controllers/videos/getOne.ts
+++ src/controllers/videos/getOne.ts
@@ -3,7 +3,7 @@ import Video from "../../models/Video";
 
 export default asyncHandler(async (req, res) => {
   const videoId = req.params.id;
-  const video = await Video.findById(videoId);
+  const video = await Video.findById(videoId).populate("_user", "name picture");
   if (video) return res.json(video);
 
   throw res.clientError("Video not found", 404);
