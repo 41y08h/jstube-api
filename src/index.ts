7ef00d0dc095014491570d570a4ffc45e55a349a
Piyush <piyush41y08h@gmail.com>
2021-05-20 11:55:18 +0530

    Wrap app in main function

--- src/index.ts
+++ src/index.ts
@@ -13,25 +13,29 @@ import morgan from "morgan";
 import fileUpload from "express-fileupload";
 import cors from "cors";
 
-const app = express();
-const debug = createDebug("app");
+async function main() {
+  const app = express();
+  const debug = createDebug("app");
 
-AuthService.configure();
+  AuthService.configure();
 
-const isDevEnv = process.env.NODE_ENV === "development";
-if (isDevEnv) app.use(morgan("dev"));
+  const isDevEnv = process.env.NODE_ENV === "development";
+  if (isDevEnv) app.use(morgan("dev"));
 
-app.use(cors());
-app.use(express.json());
-app.use(fileUpload());
-app.use(helmet());
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(parseUser);
-app.use(clientError);
-app.use(router);
-app.use(errorHandler);
+  app.use(cors());
+  app.use(express.json());
+  app.use(fileUpload());
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(passport.initialize());
+  app.use(parseUser);
+  app.use(clientError);
+  app.use(router);
+  app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
-  debug(`🚀 Ready on port ${process.env.PORT}`);
-});
+  app.listen(process.env.PORT, () => {
+    debug(`🚀 Ready on port ${process.env.PORT}`);
+  });
+}
+
+main();
