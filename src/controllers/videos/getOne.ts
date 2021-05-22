aaf083e689d7fca20d10c35a235ef3bac6d1a104
Piyush <piyush41y08h@gmail.com>
2021-05-22 14:37:25 +0530

    Fix video return value

--- src/controllers/videos/getOne.ts
+++ src/controllers/videos/getOne.ts
@@ -7,7 +7,15 @@ export default asyncHandler(async (req, res) => {
 
   const video = await prisma.video.findUnique({
     where: { id: videoId },
-    include: {
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      src: true,
+      thumbnail: true,
+      duration: true,
+      views: true,
+      uploadedAt: true,
       channel: {
         select: {
           id: true,
@@ -17,20 +25,34 @@ export default asyncHandler(async (req, res) => {
       },
     },
   });
+
+  const subscribers = await prisma.subscriber.count({
+    where: { channelId: video?.channel?.id },
+  });
+
   const likes = await prisma.videoRating.count({
     where: { videoId, status: "LIKED" },
   });
   const dislikes = await prisma.videoRating.count({
     where: { videoId, status: "DISLIKED" },
   });
+
+  const _count = {
+    channel: {
+      subscribers,
+    },
+    rating: {
+      likes,
+      dislikes,
+    },
+  };
+
   const userRating = await prisma.videoRating.findFirst({
     where: { userId, videoId },
   });
   const userRatingStatus = userRating ? userRating.status : null;
 
-  const rating = { likes, dislikes, userRatingStatus };
-
-  if (video) return res.json({ ...video, rating });
+  if (video) return res.json({ ...video, _count, userRatingStatus });
 
   throw res.clientError("Video not found", 404);
 });
