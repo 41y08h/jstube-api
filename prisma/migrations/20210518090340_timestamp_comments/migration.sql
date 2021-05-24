05b8a9792ae5d852797d34c5a8f91425d65451aa
Piyush <piyush41y08h@gmail.com>
2021-05-24 10:19:57 +0530

    Recreate db

--- prisma/migrations/20210518090340_timestamp_comments/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `editedAt` to the `Comment` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Comment" ADD COLUMN     "commentedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "editedAt" TIMESTAMP(3) NOT NULL;
--- prisma/migrations/20210519125729_add_subscriber_composite/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[channelId,userId]` on the table `Subscriber` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Subscriber.channelId_userId_unique" ON "Subscriber"("channelId", "userId");
