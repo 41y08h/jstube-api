7b118f8f548b4b30af8370a471df8f15e7a939fc
Piyush <piyush41y08h@gmail.com>
2021-05-29 19:24:52 +0530

    feat: dislike

--- src/controllers/ratings/videos/dislike.ts
+++ src/controllers/ratings/videos/dislike.ts
@@ -1,36 +1,54 @@
-import prisma from "../../../lib/prisma";
 import asyncHandler from "../../../lib/asyncHandler";
+import db from "../../../lib/db";
 
 export default asyncHandler(async (req, res) => {
   const videoId = parseInt(req.params.id);
   const userId = req.currentUser?.id as number;
 
-  const existingRating = await prisma.videoRating.findFirst({
-    where: { videoId, userId },
-  });
+  const {
+    rows: [existingRating],
+  } = await db.query(
+    `select * from "VideoRating" where "videoId" = $1 and "userId" = $2`,
+    [videoId, userId]
+  );
 
-  if (existingRating)
-    await prisma.videoRating.update({
-      where: { id: existingRating.id },
-      data: { status: "DISLIKED" },
-    });
-  else
-    await prisma.videoRating.create({
-      data: { status: "LIKED", userId, videoId },
-    });
+  if (existingRating) {
+    await db.query(
+      `
+    update "VideoRating"
+    set status = 'DISLIKED'
+    where "videoId" = $1 and
+          "userId" = $2
+    `,
+      [videoId, userId]
+    );
+  } else {
+    await db.query(
+      `
+    insert into "VideoRating"("videoId", "userId", status)
+    values ($1, $2, 'DISLIKED') returning *
+  `,
+      [videoId, userId]
+    );
+  }
 
-  const likes = await prisma.videoRating.count({
-    where: { status: "LIKED", videoId },
-  });
-  const dislikes = await prisma.videoRating.count({
-    where: { status: "DISLIKED", videoId },
-  });
-  const userRating = await prisma.videoRating.findFirst({
-    where: { userId, videoId },
-  });
-  const userRatingStatus = userRating ? userRating.status : null;
+  const {
+    rows: [ratings],
+  } = await db.query(
+    `
+    select json_build_object(
+      'likes', count((select id from "VideoRating"
+                      where "videoId" = $1 and status = 'LIKED')),
+      'dislikes', count((select id from "VideoRating"
+                      where "videoId" = $1 and status = 'DISLIKED'))
+      ) as count,
+      (select status from "VideoRating"
+      where "videoId" = $1 and
+            "userId" = $2
+      ) as "userRatingStatus"
+  `,
+    [videoId, userId]
+  );
 
-  const ratingDetail = { likes, dislikes, userRatingStatus };
-
-  res.json(ratingDetail);
+  res.json(ratings);
 });
