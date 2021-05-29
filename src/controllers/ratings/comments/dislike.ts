7b5fe89ae2ea5e9573b2c75ad1121518339576c7
Piyush <piyush41y08h@gmail.com>
2021-05-29 19:47:56 +0530

    feat: comment rating

--- src/controllers/ratings/comments/dislike.ts
+++ src/controllers/ratings/comments/dislike.ts
@@ -1,36 +1,54 @@
-import prisma from "../../../lib/prisma";
 import asyncHandler from "../../../lib/asyncHandler";
+import db from "../../../lib/db";
 
 export default asyncHandler(async (req, res) => {
   const commentId = parseInt(req.params.id);
   const userId = req.currentUser?.id as number;
 
-  const existingRating = await prisma.commentRating.findFirst({
-    where: { commentId, userId },
-  });
+  const {
+    rows: [existingRating],
+  } = await db.query(
+    `select * from "CommentRating" where "commentId" = $1 and "userId" = $2`,
+    [commentId, userId]
+  );
 
-  if (existingRating)
-    await prisma.commentRating.update({
-      where: { id: existingRating.id },
-      data: { status: "DISLIKED" },
-    });
-  else
-    await prisma.commentRating.create({
-      data: { status: "LIKED", userId, commentId },
-    });
+  if (existingRating) {
+    await db.query(
+      `
+    update "CommentRating"
+    set status = 'DISLIKED'
+    where "commentId" = $1 and
+          "userId" = $2
+    `,
+      [commentId, userId]
+    );
+  } else {
+    await db.query(
+      `
+    insert into "CommentRating"("commentId", "userId", status)
+    values ($1, $2, 'DISLIKED') returning *
+  `,
+      [commentId, userId]
+    );
+  }
 
-  const likes = await prisma.commentRating.count({
-    where: { status: "LIKED", commentId },
-  });
-  const dislikes = await prisma.commentRating.count({
-    where: { status: "DISLIKED", commentId },
-  });
-  const userRating = await prisma.commentRating.findFirst({
-    where: { userId, commentId },
-  });
-  const userRatingStatus = userRating ? userRating.status : null;
+  const {
+    rows: [ratings],
+  } = await db.query(
+    `
+    select json_build_object(
+      'likes', count((select id from "CommentRating"
+                      where "commentId" = $1 and status = 'LIKED')),
+      'dislikes', count((select id from "CommentRating"
+                      where "commentId" = $1 and status = 'DISLIKED'))
+      ) as count,
+      (select status from "CommentRating"
+      where "commentId" = $1 and
+            "userId" = $2
+      ) as "userRatingStatus"
+  `,
+    [commentId, userId]
+  );
 
-  const ratingDetail = { likes, dislikes, userRatingStatus };
-
-  res.json(ratingDetail);
+  res.json(ratings);
 });
