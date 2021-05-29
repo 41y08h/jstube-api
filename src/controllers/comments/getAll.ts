9e7c3d3731985b25e8079c5a793062f61f5b7841
Piyush <piyush41y08h@gmail.com>
2021-05-29 21:27:55 +0530

    feat: comments query

--- src/controllers/comments/getAll.ts
+++ src/controllers/comments/getAll.ts
@@ -1,31 +1,44 @@
 import asyncHandler from "../../lib/asyncHandler";
-import prisma from "../../lib/prisma";
+import db from "../../lib/db";
 
 export default asyncHandler(async (req, res) => {
   const videoId = parseInt(req.params.videoId);
+  const page = parseInt(req.query.page as string) || 1;
 
-  const comments = await prisma.comment.findMany({
-    where: {
-      videoId,
-      replyToCommentId: null,
-    },
-    select: {
-      id: true,
-      text: true,
-      author: {
-        select: {
-          id: true,
-          name: true,
-          picture: true,
-        },
-      },
-      _count: {
-        select: {
-          replies: true,
-        },
-      },
-    },
+  const { rows: comments } = await db.query(
+    `
+    select "Comment".id, 
+           text,
+           "userId",
+           "videoId",
+           "createdAt",
+           "updatedAt",
+           to_jsonb(author) as author 
+    from "Comment"
+    left join "PublicUser" as author on author.id = "userId"
+    where "videoId" = $1
+    order by id
+    offset ($2 - 1)* 10
+    limit 10
+  `,
+    [videoId, page]
+  );
+
+  const {
+    rows: [{ count: total }],
+  } = await db.query(
+    `
+    select cast(count("Comment") as int) from "Comment" 
+    where "videoId" = $1
+    `,
+    [videoId]
+  );
+
+  res.json({
+    total,
+    current: comments.length,
+    page,
+    hasMore: page * 10 < total,
+    comments,
   });
-
-  res.json(comments);
 });
