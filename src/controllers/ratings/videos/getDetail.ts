c39d3897e6491748b3ff37af17b138c02e82425b
Piyush <piyush41y08h@gmail.com>
2021-05-29 19:26:14 +0530

    feat: video ratings status

--- src/controllers/ratings/videos/getDetail.ts
+++ src/controllers/ratings/videos/getDetail.ts
@@ -1,22 +1,26 @@
 import asyncHandler from "../../../lib/asyncHandler";
-import prisma from "../../../lib/prisma";
+import db from "../../../lib/db";
 
 export default asyncHandler(async (req, res) => {
   const videoId = parseInt(req.params.id);
   const userId = req.currentUser?.id;
+  const {
+    rows: [ratings],
+  } = await db.query(
+    `
+    select json_build_object(
+      'likes', count((select id from "VideoRating"
+                      where "videoId" = $1 and status = 'LIKED')),
+      'dislikes', count((select id from "VideoRating"
+                      where "videoId" = $1 and status = 'DISLIKED'))
+      ) as count,
+      (select status from "VideoRating"
+      where "videoId" = $1 and
+            "userId" = $2
+      ) as "userRatingStatus"
+  `,
+    [videoId, userId]
+  );
 
-  const likes = await prisma.videoRating.count({
-    where: { status: "LIKED", videoId },
-  });
-  const dislikes = await prisma.videoRating.count({
-    where: { status: "DISLIKED", videoId },
-  });
-  const userRating = await prisma.videoRating.findFirst({
-    where: { userId, videoId },
-  });
-  const userRatingStatus = userRating ? userRating.status : null;
-
-  const ratingDetail = { likes, dislikes, userRatingStatus };
-
-  res.json(ratingDetail);
+  res.json(ratings);
 });
