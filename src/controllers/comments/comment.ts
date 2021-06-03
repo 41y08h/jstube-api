8ac6d9bd04906c4cf1edb3622a1e1241d3c6cf51
Piyush <piyush41y08h@gmail.com>
2021-06-03 12:00:40 +0530

    fix comment handlers

--- src/controllers/comments/comment.ts
+++ src/controllers/comments/comment.ts
@@ -9,37 +9,48 @@ export default asyncHandler(async (req, res) => {
 
   const {
     rows: [comment],
+    rowCount,
   } = await db.query(
     `
-    with inserted as ( insert into "Comment"(text, "videoId", "userId")
-    values ($1, $2, $3)
-    returning *)
-    select inserted.*, 
-	   to_json(author) as author,
-	   jsonb_build_object(
-           'count', json_build_object(
-               'likes', count("CommentLikes"),
-               'dislikes', count("CommentDislikes")
-           ),
+    with inserted as (insert into "Comment"(text, "videoId", "userId")
+    values ($1, $2, $3) returning *)
+    select inserted.*,
+          to_json(author) as author,
+          json_build_object(
+          'count', json_build_object(
+            'likes', count(distinct "cLikes"),
+            'dislikes', count(distinct "cDislikes")
+          ),
           'userRatingStatus', (
-             select status
-             from "CommentRating"
-             where "userId" = $3  and
-                   "commentId" = inserted.id
+              select status
+              from "CommentRating"
+              where "commentId" = inserted.id and
+                    "userId" = $3
           )
-        ) as ratings
+      ) as ratings
     from inserted
-    left join "PublicUser" as author on
-	inserted."userId" = author.id
-    left join (select *, count("commentId") as count from "CommentRating" where status = 'LIKED' group by "CommentRating".id) as "CommentLikes" on
-        "CommentLikes"."commentId" = inserted.id
-    left join (select *, count("commentId") as count from "CommentRating" where status = 'DISLIKED' group by "CommentRating".id) as "CommentDislikes" on
-        "CommentDislikes"."commentId" = inserted.id
-    group by inserted.id, inserted.text, inserted."originalCommentId", inserted."replyToCommentId",
-         inserted."userId", inserted."videoId", inserted."createdAt", inserted."updatedAt", author.*
+    left join "PublicUser" author on
+        author.id = inserted."userId"
+    left join "CommentRating" "cLikes" on
+        "cLikes"."commentId" = inserted.id and
+        "cLikes".status = 'LIKED'
+    left join "CommentRating" "cDislikes" on
+        "cDislikes"."commentId" = inserted.id and
+        "cDislikes".status = 'DISLIKED'
+    group by inserted.id,
+            inserted.text,
+            inserted."originalCommentId",
+            inserted."replyToCommentId",
+            inserted."userId",
+            inserted."videoId",
+            inserted."createdAt",
+            inserted."updatedAt",
+            author.*
     `,
     [req.body.text, videoId, userId]
   );
+
+  if (!rowCount) throw res.clientError("There was a problem", 422);
 
   res.json(comment);
 });
--- src/controllers/comments/edit.ts
+++ src/controllers/comments/edit.ts
@@ -20,13 +20,40 @@ export default asyncHandler(async (req, res) => {
       where id = $2 and "userId" = $3
       returning *
     )
-    select updated.*, 
-           to_json(author) as author 
+    select updated.*,
+          to_json(author) as author,
+          json_build_object(
+          'count', json_build_object(
+            'likes', count(distinct "cLikes"),
+            'dislikes', count(distinct "cDislikes")
+          ),
+          'userRatingStatus', (
+              select status
+              from "CommentRating"
+              where "commentId" = updated.id and
+                    "userId" = $3
+          )
+      ) as ratings
     from updated
     left join "PublicUser" author on
-      author.id = updated."userId";
-  `,
-    [text, commentId, userId]
+        author.id = updated."userId"
+    left join "CommentRating" "cLikes" on
+        "cLikes"."commentId" = updated.id and
+        "cLikes".status = 'LIKED'
+    left join "CommentRating" "cDislikes" on
+        "cDislikes"."commentId" = updated.id and
+        "cDislikes".status = 'DISLIKED'
+    group by updated.id,
+            updated.text,
+            updated."originalCommentId",
+            updated."replyToCommentId",
+            updated."userId",
+            updated."videoId",
+            updated."createdAt",
+            updated."updatedAt",
+            author.*
+    `,
+    [req.body.text, commentId, userId]
   );
 
   if (!rowCount) throw res.clientError("There was a problem", 422);
