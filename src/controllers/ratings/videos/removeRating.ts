9851e476b14d9bb2fb5c2f0620ccc72df563527e
Piyush <piyush41y08h@gmail.com>
2021-06-03 08:53:20 +0530

    fix video Rating removal

--- src/controllers/ratings/videos/removeRating.ts
+++ src/controllers/ratings/videos/removeRating.ts
@@ -1,6 +1,5 @@
 import asyncHandler from "../../../lib/asyncHandler";
 import db from "../../../lib/db";
-import prisma from "../../../lib/prisma";
 
 export default asyncHandler(async (req, res) => {
   const videoId = parseInt(req.params.id);
@@ -22,15 +21,24 @@ export default asyncHandler(async (req, res) => {
   } = await db.query(
     `
     select json_build_object(
-      'likes', count((select id from "VideoRating"
-                      where "videoId" = $1 and status = 'LIKED')),
-      'dislikes', count((select id from "VideoRating"
-                      where "videoId" = $1 and status = 'DISLIKED'))
-      ) as count,
-      (select status from "VideoRating"
-      where "videoId" = $1 and
-            "userId" = $2
-      ) as "userRatingStatus"
+      'count', json_build_object(
+          'likes', count(distinct "vLikes"),
+          'dislikes', count(distinct "vDislikes")
+          ),
+      'userRatingStatus', (
+              select status
+              from "VideoRating"
+              where "videoId" = $1
+                and "userId" = $2
+      )
+      ) as ratings
+    from "VideoRating"
+    left join "VideoRating" "vLikes" on
+      "vLikes"."videoId" = $1 and
+      "vLikes".status = 'LIKED'
+    left join "VideoRating" "vDislikes" on
+      "vDislikes"."videoId" = $1 and
+      "vDislikes".status = 'DISLIKED'
   `,
     [videoId, userId]
   );
