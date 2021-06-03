4d646eb8c58581e9d6cdeb5875f09b0e4b6267f0
Piyush <piyush41y08h@gmail.com>
2021-06-03 15:28:10 +0530

    fix: reply controller

--- src/controllers/comments/reply.ts
+++ src/controllers/comments/reply.ts
@@ -1,6 +1,5 @@
 import asyncHandler from "../../lib/asyncHandler";
 import db from "../../lib/db";
-import prisma from "../../lib/prisma";
 
 export default asyncHandler(async (req, res) => {
   const commentId = parseInt(req.params.id);
@@ -35,15 +34,43 @@ export default asyncHandler(async (req, res) => {
     rows: [reply],
   } = await db.query(
     `
-    insert into "Comment"
-    (text, 
-    "userId", 
-    "videoId", 
-    "replyToCommentId",
-    "originalCommentId")
-    values ($1, $2, $3, $4, $5)
-    returning *
-  `,
+    with inserted as (
+      insert into "Comment"(text, "userId", "videoId", "replyToCommentId", "originalCommentId")
+      values ($1, $2, $3, $4, $5) returning *
+    )
+    select inserted.*,
+          to_json(author) as author,
+          json_build_object(
+          'count', json_build_object(
+            'likes', count(distinct "cLikes"),
+            'dislikes', count(distinct "cDislikes")
+          ),
+          'userRatingStatus', (
+              select status
+              from "CommentRating"
+              where "commentId" = inserted.id and
+                    "userId" = $2
+          )
+      ) as ratings
+    from inserted
+    left join "PublicUser" author on
+        author.id = inserted."userId"
+    left join "CommentRating" "cLikes" on
+        "cLikes"."commentId" = inserted.id and
+        "cLikes".status = 'LIKED'
+    left join "CommentRating" "cDislikes" on
+        "cDislikes"."commentId" = inserted.id and
+        "cDislikes".status = 'DISLIKED'
+    group by inserted.id,
+          inserted.text,
+          inserted."originalCommentId",
+          inserted."replyToCommentId",
+          inserted."userId",
+          inserted."videoId",
+          inserted."createdAt",
+          inserted."updatedAt",
+          author.*
+    `,
     [
       text,
       userId,
