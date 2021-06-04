6ab10b8fbca8791a8edc8e2990576312dcdbf269
Piyush <piyush41y08h@gmail.com>
2021-06-04 13:48:20 +0530

    query from view

--- src/controllers/comments/edit.ts
+++ src/controllers/comments/edit.ts
@@ -5,58 +5,57 @@ export default asyncHandler(async (req, res) => {
   const commentId = parseInt(req.params.id);
   const userId = req.currentUser?.id as number;
 
-  const { text } = req.body;
-
-  if (!text) throw res.clientError("Text field is required.", 422);
+  if (!req.body.text) throw res.clientError("Text is a required field.");
 
   const {
-    rows: [comment],
+    rows: [updatedComment],
     rowCount,
   } = await db.query(
     `
-    with updated as (
-      update "Comment"
-      set text = $1
-      where id = $2 and "userId" = $3
-      returning *
-    )
-    select updated.*,
-          to_json(author) as author,
-          json_build_object(
-          'count', json_build_object(
-            'likes', count(distinct "cLikes"),
-            'dislikes', count(distinct "cDislikes")
-          ),
-          'userRatingStatus', (
-              select status
-              from "CommentRating"
-              where "commentId" = updated.id and
-                    "userId" = $3
-          )
-      ) as ratings
-    from updated
-    left join "PublicUser" author on
-        author.id = updated."userId"
-    left join "CommentRating" "cLikes" on
-        "cLikes"."commentId" = updated.id and
-        "cLikes".status = 'LIKED'
-    left join "CommentRating" "cDislikes" on
-        "cDislikes"."commentId" = updated.id and
-        "cDislikes".status = 'DISLIKED'
-    group by updated.id,
-            updated.text,
-            updated."originalCommentId",
-            updated."replyToCommentId",
-            updated."userId",
-            updated."videoId",
-            updated."createdAt",
-            updated."updatedAt",
-            author.*
+    update "Comment"
+    set text = $1
+    where id = $2 and "userId" = $3
+    returning id, "userId" 
     `,
     [req.body.text, commentId, userId]
   );
 
   if (!rowCount) throw res.clientError("There was a problem", 422);
+
+  const {
+    rows: [comment],
+  } = await db.query(
+    `
+    select id,
+        text,
+        "originalCommentId",
+        "replyToCommentId",
+        "userId",
+        "videoId",
+        "createdAt",
+        "updatedAt",
+        json_build_object(
+        'id', "authorId",
+        'name', "authorName",
+        'picture', "authorPicture"
+        ) as author,
+        json_build_object(
+            'count', json_build_object(
+                'likes', "likesCount",
+                'dislikes', "dislikesCount"
+            ),
+            'userRatingStatus', (
+              select status
+              from "CommentRating"
+              where "commentId" = "JoinedComment".id and
+                    "userId" = $2
+          )
+        ) as ratings,
+        "replyCount"
+    from "JoinedComment" where id = $1
+    `,
+    [updatedComment.id, updatedComment.userId]
+  );
 
   res.json(comment);
 });
