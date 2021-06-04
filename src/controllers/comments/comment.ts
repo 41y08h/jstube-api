2004326e95182cbcc205128c19ef760fb5abc433
Piyush <piyush41y08h@gmail.com>
2021-06-04 13:40:34 +0530

    query from view

--- src/controllers/comments/comment.ts
+++ src/controllers/comments/comment.ts
@@ -8,49 +8,52 @@ export default asyncHandler(async (req, res) => {
   if (!req.body.text) throw res.clientError("Text is a required field.");
 
   const {
-    rows: [comment],
+    rows: [createdComment],
     rowCount,
   } = await db.query(
     `
-    with inserted as (insert into "Comment"(text, "videoId", "userId")
-    values ($1, $2, $3) returning *)
-    select inserted.*,
-          to_json(author) as author,
-          json_build_object(
-          'count', json_build_object(
-            'likes', count(distinct "cLikes"),
-            'dislikes', count(distinct "cDislikes")
-          ),
-          'userRatingStatus', (
-              select status
-              from "CommentRating"
-              where "commentId" = inserted.id and
-                    "userId" = $3
-          )
-      ) as ratings
-    from inserted
-    left join "PublicUser" author on
-        author.id = inserted."userId"
-    left join "CommentRating" "cLikes" on
-        "cLikes"."commentId" = inserted.id and
-        "cLikes".status = 'LIKED'
-    left join "CommentRating" "cDislikes" on
-        "cDislikes"."commentId" = inserted.id and
-        "cDislikes".status = 'DISLIKED'
-    group by inserted.id,
-            inserted.text,
-            inserted."originalCommentId",
-            inserted."replyToCommentId",
-            inserted."userId",
-            inserted."videoId",
-            inserted."createdAt",
-            inserted."updatedAt",
-            author.*
+    insert into "Comment"(text, "videoId", "userId")
+    values ($1, $2, $3) returning id, "userId"   
     `,
     [req.body.text, videoId, userId]
   );
 
   if (!rowCount) throw res.clientError("There was a problem", 422);
+
+  const {
+    rows: [comment],
+  } = await db.query(
+    `
+    select id,
+        text,
+        "originalCommentId",
+        "replyToCommentId",
+        "userId",
+        "videoId",
+        "createdAt",
+        "updatedAt",
+        json_build_object(
+        'id', "authorId",
+        'name', "authorName",
+        'picture', "authorPicture"
+        ) as author,
+        json_build_object(
+            'count', json_build_object(
+                'likes', "likesCount",
+                'dislikes', "dislikesCount"
+            ),
+            'userRatingStatus', (
+              select status
+              from "CommentRating"
+              where "commentId" = "JoinedComment".id and
+                    "userId" = $2
+          )
+        ) as ratings,
+        "replyCount"
+    from "JoinedComment" where id = $1
+    `,
+    [createdComment.id, createdComment.userId]
+  );
 
   res.json(comment);
 });
