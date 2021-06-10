f4d6ac8f96dcd5f8ad0b53e8ee39883e5b0510bf
Piyush <piyush41y08h@gmail.com>
2021-06-10 20:20:45 +0530

    add subscription list route

--- src/routes/subscribers.ts
+++ src/routes/subscribers.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import SubscribersController from "../controllers/subscribers";
+import db from "../lib/db";
 import authenticate from "../middlewares/authenticate";
 
 const subscribers = Router();
@@ -9,5 +10,21 @@ subscribers
   .route("/:channelId")
   .post(SubscribersController.subscribe)
   .delete(SubscribersController.unsubscribe);
+
+subscribers.get("/subscriptions", async (req, res) => {
+  const userId = req.currentUser?.id;
+
+  const { rows } = await db.query(
+    `
+  select "Subscriber".*, to_json(channel) as channel from "Subscriber"
+  left join "PublicUser" as channel on
+  id = "channelId"
+  where "userId" = $1  
+  `,
+    [userId]
+  );
+
+  res.json(rows);
+});
 
 export default subscribers;
