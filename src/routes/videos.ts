1b2ffa11d35347394ef985ac79e4c8467aed8c4d
Piyush <piyush41y08h@gmail.com>
2021-06-11 23:09:00 +0530

    feat: user video retrieval

--- src/routes/videos.ts
+++ src/routes/videos.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import VideosController from "../controllers/videos";
+import db from "../lib/db";
 import authenticate from "../middlewares/authenticate";
 
 const videos = Router();
@@ -8,6 +9,21 @@ videos
   .route("/")
   .get(VideosController.getAll)
   .post(authenticate, VideosController.upload);
+
+videos.get("/mine", async (req, res) => {
+  const userId = req.currentUser?.id;
+  const { rows: videos } = await db.query(
+    `
+  select "Video".*, to_json(channel) channel from "Video"
+  left join "PublicUser" channel on 
+    channel.id = "userId"
+  where "userId" = $1
+  `,
+    [userId]
+  );
+
+  res.json(videos);
+});
 
 videos.get("/:id", VideosController.getOne);
 
