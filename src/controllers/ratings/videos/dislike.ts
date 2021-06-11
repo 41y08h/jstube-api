30b525b87e04d4732953b47256b2d97911fb2e95
Piyush <piyush41y08h@gmail.com>
2021-06-11 11:55:19 +0530

    feat: liked playlist

--- src/controllers/ratings/videos/dislike.ts
+++ src/controllers/ratings/videos/dislike.ts
@@ -37,17 +37,15 @@ export default asyncHandler(async (req, res) => {
   } = await db.query(
     `
     select json_build_object(
-      'count', json_build_object(
           'likes', count(distinct "vLikes"),
           'dislikes', count(distinct "vDislikes")
-          ),
-      'userRatingStatus', (
-              select status
-              from "VideoRating"
-              where "videoId" = $1
-                and "userId" = $2
-      )
-      ) as ratings
+       ) as count,
+      (
+        select status
+        from "VideoRating"
+        where "videoId" = $1
+          and "userId" = $2
+      ) as "userRatingStatus"
     from "VideoRating"
     left join "VideoRating" "vLikes" on
       "vLikes"."videoId" = $1 and
--- src/controllers/ratings/videos/like.ts
+++ src/controllers/ratings/videos/like.ts
@@ -31,23 +31,20 @@ export default asyncHandler(async (req, res) => {
       [videoId, userId]
     );
   }
-
   const {
     rows: [ratings],
   } = await db.query(
     `
     select json_build_object(
-      'count', json_build_object(
           'likes', count(distinct "vLikes"),
           'dislikes', count(distinct "vDislikes")
-          ),
-      'userRatingStatus', (
-              select status
-              from "VideoRating"
-              where "videoId" = $1
-                and "userId" = $2
-      )
-      ) as ratings
+       ) as count,
+      (
+        select status
+        from "VideoRating"
+        where "videoId" = $1
+          and "userId" = $2
+      ) as "userRatingStatus"
     from "VideoRating"
     left join "VideoRating" "vLikes" on
       "vLikes"."videoId" = $1 and
