c6a9bf8090dcbf9c677bb339592f473225e2b245
Piyush <piyush41y08h@gmail.com>
2021-06-12 13:42:52 +0530

    feat: history

---
+++ src/routes/history.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import db from "../lib/db";
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const userId = req.currentUser?.id;
+
+  const query = await db.query(
+    `
+    select to_json(v) as video, "viewedAt" from "History"
+    left join "VideoWithChannel" v on
+        v.id = "History"."videoId"
+    where "History"."userId" = $1;
+    `,
+    [userId]
+  );
+
+  res.json(query.rows);
+});
+
+export default router;
