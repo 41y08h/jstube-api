e52ef1a372bf6aedf15e0847b43bda3bac5abc13
Piyush <piyush41y08h@gmail.com>
2021-07-02 18:25:47 +0530

    fix seeder to use raw sql

--- src/seeder/index.ts
+++ src/seeder/index.ts
@@ -1,26 +1,36 @@
 import "dotenv/config";
-import videosData from "./videosData";
+import db from "../lib/db";
 import createDebug from "debug";
-import prisma from "../lib/prisma";
+import videosData from "./videos.data";
+import clearConsole from "../lib/clearConsole";
 
-(async () => {
+async function main() {
   const debug = createDebug("app:seeder");
 
   const videosSeeds = videosData.map(async (videoData) => {
-    const video = await prisma.video.create({
-      data: {
-        title: videoData.title,
-        description: videoData.description,
-        thumbnail: videoData.thumbnail,
-        src: videoData.src,
-        duration: videoData.duration,
-        userId: 1,
-      },
-    });
+    const {
+      rows: [video],
+    } = await db.query(
+      `
+    insert into "Video"
+    (title, description, thumbnail, src, duration, "userId")
+    values ($1, $2, $3, $4, $5, $6)
+    returning *
+    `,
+      [
+        videoData.title,
+        videoData.description,
+        videoData.thumbnail,
+        videoData.src,
+        videoData.duration,
+        1,
+      ]
+    );
     debug(`🌱 Added video ${video.title}`);
   });
 
   await Promise.all(videosSeeds);
+}
 
-  process.exit();
-})();
+clearConsole();
+main().then(() => process.exit());
