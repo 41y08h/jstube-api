35681325a1c2f965c62f40a43426b7b998ca814e
Piyush <piyush41y08h@gmail.com>
2021-07-02 18:09:41 +0530

    fix video list order

--- src/controllers/videos/getAll.ts
+++ src/controllers/videos/getAll.ts
@@ -2,16 +2,19 @@ import asyncHandler from "../../lib/asyncHandler";
 import db from "../../lib/db";
 
 export default asyncHandler(async (req, res) => {
-  const { rows: videos } = await db.query(`
-      select "Video".*,
-           json_build_object(
-            'id', "Channel".id,
-            'name', "Channel".name,
-            'picture', "Channel".picture
-           ) as channel
-    from "Video"
-    left join "User" as "Channel" on
-      "Channel".id = "Video"."userId"
-  `);
+  const { rows: videos } = await db.query(
+    `
+  select "Video".*,
+          json_build_object(
+          'id', "Channel".id,
+          'name', "Channel".name,
+          'picture', "Channel".picture
+          ) as channel
+  from "Video"
+  left join "User" as "Channel" on
+    "Channel".id = "Video"."userId"
+  order by "uploadedAt" desc
+  `
+  );
   res.json(videos);
 });
