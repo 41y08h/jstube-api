e3139bd5645fbf39892289301980854a6a91371e
Piyush <piyush41y08h@gmail.com>
2021-07-09 14:53:48 +0530

    Improve pagination with cursor based pagination

--- src/controllers/comments/getAll.ts
+++ src/controllers/comments/getAll.ts
@@ -4,7 +4,9 @@ import db from "../../lib/db";
 export default asyncHandler(async (req, res) => {
   const videoId = parseInt(req.params.videoId);
   const userId = req.currentUser?.id;
-  const page = parseInt(req.query.page as string) || 1;
+  const beforeId = req.query.beforeId
+    ? parseInt(req.query.beforeId as string)
+    : undefined;
 
   const { rows: comments } = await db.query(
     `
@@ -36,12 +38,11 @@ export default asyncHandler(async (req, res) => {
     "replyCount"
     from "JoinedComment"
     where "replyToCommentId" is null and 
-          "videoId" = $1
-    order by id
-    offset ($3 - 1) * 10
+          "videoId" = $1 ${beforeId ? `and id < $3` : ``}
+    order by id desc
     limit 10
   `,
-    [videoId, userId, page]
+    [videoId, userId, beforeId].filter(Boolean)
   );
 
   const {
@@ -54,11 +55,21 @@ export default asyncHandler(async (req, res) => {
     [videoId]
   );
 
+  const {
+    rows: [{ hasMore }],
+  } = await db.query(
+    `
+    select (count("Comment") != 0) as "hasMore" from "Comment"
+    where "videoId" = $1 and 
+          "replyToCommentId" is null and
+          id < $2
+    `,
+    [videoId, comments[comments.length - 1]?.id]
+  );
+
   res.json({
     total,
-    count: comments.length,
-    page,
-    hasMore: page * 10 < total,
+    hasMore,
     items: comments,
   });
 });
